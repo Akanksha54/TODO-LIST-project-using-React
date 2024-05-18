60cbc02490ad90eb304f1995b3f24225d7c68274
Akanksha54 <122151315+Akanksha54@users.noreply.github.com>
2024-05-18 13:45:22 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const About = () => {
+    const centerStyle = {
+        textAlign: 'center'
+    };
+
+    return (
+        <div>
+            <h1 style={centerStyle}>
+               THIS IS A TODO LIST
+            </h1>
+            <p style={centerStyle}>Developed by Akanksha Pise in REACTJS</p>
+            <p>Github Profile</p>
+        </div>
+    );
+}
